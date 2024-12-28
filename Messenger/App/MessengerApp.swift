73563acb3514424d5e84a5d4eca88b7b2148e4e7
mohak92 <tamhane.mohak@gmail.com>
2024-12-28 14:27:30 -0500

    login view

---
+++ Messenger/App/MessengerApp.swift
@@ -0,0 +1,17 @@
+//
+//  MessengerApp.swift
+//  Messenger
+//
+//  Created by Mohak Tamhane on 12/27/24.
+//
+
+import SwiftUI
+
+@main
+struct MessengerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
