975e8641f2517d8331bbcdfd2b1ac80a7e52801a
mohak92 <tamhane.mohak@gmail.com>
2024-12-30 15:36:14 -0500

    new message view

--- Messenger/Core/NewMessage/NewMessageView.swift
+++ Messenger/Core/NewMessage/NewMessageView.swift
@@ -8,11 +8,60 @@
 import SwiftUI
 
 struct NewMessageView: View {
+    
+    @State private var searchText = ""
+    @Environment(\.dismiss) var dismiss
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationStack {
+            ScrollView {
+                TextField("To: ", text: $searchText)
+                    .frame(height: 44)
+                    .padding(.leading)
+                    .background(Color(.systemGroupedBackground))
+                
+                Text("CONTACTS")
+                    .foregroundStyle(Color.gray)
+                    .font(.footnote)
+                    .frame(maxWidth: .infinity, alignment: .leading)
+                    .padding()
+                
+                ForEach( 0 ... 10, id: \.self) { user in
+                    VStack {
+                        HStack {
+                            Image(systemName: "person.circle.fill")
+                                .resizable()
+                                .frame(width: 32, height: 32)
+                                .foregroundStyle(Color(.systemGray4))
+                            
+                            Text("Chadwick Bozeman")
+                                .font(.subheadline)
+                                .fontWeight(.semibold)
+                            
+                            Spacer()
+                        }
+                        .padding(.leading)
+                        
+                        Divider()
+                            .padding(.leading, 40)
+                    }
+                }
+            }
+            .navigationTitle("New Message")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarLeading) {
+                    Button("Cancel") {
+                        dismiss()
+                    }
+                    .foregroundStyle(Color.black)
+                }
+            }
+        }
     }
 }
 
 #Preview {
-    NewMessageView()
+    NavigationStack {
+        NewMessageView()
+    }
 }
